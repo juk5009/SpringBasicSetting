0d62dfa87fcc0d1f4541c3aa966f4e16b92a5d7f
Kjuk <juk6340@gmail.com>
2023-02-02 09:07:21 +0900

    spring 기본세팅 완료

---
+++ src/test/java/shop/mtcoding/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
